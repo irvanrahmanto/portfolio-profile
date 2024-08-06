b48e08573d718a5a94df13ef73d9f6496d1be3d0
irvan <irvan.rahmanto123@gmail.com>
2024-08-06 15:18:06 +0700

    feat: handling color

--- src/components/section/Hero.jsx
+++ src/components/section/Hero.jsx
@@ -1,8 +1,10 @@
 const Hero = () => {
   return(
-    <>
-        <h1>This is Hero!</h1>
-    </>
+    <section className="bg-white">
+      <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        
+      </div>
+    </section>
   );
 };
 
