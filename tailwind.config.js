bf70c52735e95b10128bb707161a2d605a6442a5
irvan <irvan.rahmanto123@gmail.com>
2024-08-22 15:54:11 +0700

    feat: add hamburger animation & certificate

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+import flowbite from "flowbite/plugin"; 
+
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {}
   },
-  plugins: [],
+  plugins: [
+    flowbite
+  ],
 }
 
